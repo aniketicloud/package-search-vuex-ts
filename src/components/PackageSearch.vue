e5989f0baf76fab87007968a827a818e373b718b
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-21 12:36:58 +0530

    some code clean up

---
+++ src/components/PackageSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Search NPM Packges</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PackageSearch'
+});
+</script>
+
+<style scoped></style>
