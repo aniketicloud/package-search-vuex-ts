f044f18b2e370c1f68c753a0a9fd31573d03818a
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-21 13:46:20 +0530

    basic implementation of search packages is completed

--- src/components/PackageSearch.vue
+++ src/components/PackageSearch.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>Search NPM Packges</div>
+  <div>
+    <h1>Search NPM Packges</h1>
+    <input type="search" v-model="searchterm" />
+    <button type="submit" @click="fetchPackageDetails(searchterm)">
+      Search
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapActions } from 'vuex';
 
 export default defineComponent({
-  name: 'PackageSearch'
+  name: 'PackageSearch',
+  data() {
+    return {
+      searchterm: ''
+    };
+  },
+
+  methods: {
+    ...mapActions('packagesModule', {
+      fetchPackageDetails: 'fetchPackages'
+    })
+  }
 });
 </script>
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,51 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
-export default createStore({
-  state: {
-  },
+const npm_search_url: string = 'https://registry.npmjs.org/-/v1/search';
+
+const packagesModule = {
+  namespaced: true,
+  state: () => ({
+    packages: null,
+    loading: null,
+    error: null
+    // packages: Object,
+    // loading: Boolean,
+    // error: String
+  }),
   getters: {
+    getPackageNames(state: any) {
+      return state.packages;
+    }
   },
   mutations: {
+    setPackages(state: any, packagesData: any) {
+      state.packages = packagesData;
+    }
   },
   actions: {
-  },
-  modules: {
+    async fetchPackages({ commit }: any, searchParam: string) {
+      try {
+        const { data } = await axios.get(npm_search_url, {
+          params: {
+            text: searchParam
+          }
+        });
+
+        commit('setPackages', data);
+      } catch (error) {
+        console.log('Error: ', error);
+      }
+    }
   }
-})
+};
+
+export default createStore({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    packagesModule: packagesModule
+  }
+});
