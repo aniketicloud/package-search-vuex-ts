f22a1426ef02abcda57d6f10e3161343f00d3f3a
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-23 13:00:25 +0530

    removed unneccessary code

--- src/store/modules/packages/state.ts
+++ src/store/modules/packages/state.ts
@@ -1,10 +1,5 @@
 import { PackagesState } from '@/constants/store';
 
-export interface PackagesStateInterface {
-  packages: string[];
-}
-
 export const state = {
-  [PackagesState.PACKAGE_DETAILS]: null,
-  // [PackagesState.PACKAGE_NAMES]: []
+  [PackagesState.PACKAGE_DETAILS]: null
 };
