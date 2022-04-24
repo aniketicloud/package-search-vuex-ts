1f9259cd6db8b0c5aea12a227653238ee5aa3357
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-04-24 15:25:13 +0530

    Added strict mode in vuex store. state will mutated with only mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,6 +7,8 @@ import { packagesModule } from './modules/packages';
 import { Modules } from '@/constants/store';
 
 export default createStore({
+  // strict mode: in development, state will not be mutated outside of mutation handlers
+  strict: process.env.NODE_ENV !== 'production',
   state: {},
   getters: {},
   mutations: {},
